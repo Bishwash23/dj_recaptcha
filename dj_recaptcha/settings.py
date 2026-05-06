b07b0f6c57c23432e552c0410e76e3ebc6028378
Bishwash23 <bishwashb23@gmail.com>
2026-05-06 15:58:00 +0545

    Add RECAPTCHA keys to settings.py

--- dj_recaptcha/settings.py
+++ dj_recaptcha/settings.py
@@ -12,6 +12,9 @@ https://docs.djangoproject.com/en/6.0/ref/settings/
 
 from pathlib import Path
 
+RECAPTCHA_PUBLIC_KEY = '6LfF6dssAAAAALAH1SuJuIADC2RsctO3at1YR9TC'
+RECAPTCHA_PRIVATE_KEY = '6LfF6dssAAAAAEugQhrmQttxiE7u_hZrQmIxGD1W'
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
